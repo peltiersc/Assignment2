74c9cdc893048fdcd0ad336ba3c4d08aaa22d939
peltiersc <punkyhoya@gmail.com>
2015-03-25 17:48:05 -0400

    Comment in First View Controller
    thanks!

--- src/Lesson02/FirstViewController.swift
+++ src/Lesson02/FirstViewController.swift
@@ -17,3 +17,5 @@ class FirstViewController: UIViewController {
     TODO four: Hook up the button to a NEW function (in additino to the three above). Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
     */
 }
+
+// Hi Dustin, thanks for helping me!
